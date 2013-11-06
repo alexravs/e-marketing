bec81d6de2c1d3f6750b1ce547d70ad9a6d54794
Alex Ravet <ravet.alexandre@gmail.com>
2013-11-06 20:11:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EMarketing::Application.config.secret_key_base = 'ee699cb378015bea689482fbddfde010f5563edc7d8b56e0eb361990f61e6045c27f393feabfa40e06adbb65a3897ca62289cc922a923f8ef16ae46c794c2aca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EMarketing::Application.config.session_store :cookie_store, key: '_e-marketing_session'
